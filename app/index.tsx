cb95b037eb41b7b0de7617eabf1759f7e587f5c5
JScoder4005 <muk4005@gmail.com>
2025-10-03 00:16:47 +0530

    Add font loading and update layout; implement offers list with icons

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,20 @@
-import { Text, View } from "react-native";
+import { offers } from "@/constants";
+import { FlatList, Pressable, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import "./global.css";
  
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-2xl font-bold text-red-700">
-        Hello World!
-      </Text>
-    </View>
+    <SafeAreaView>
+      <FlatList data={offers} renderItem={({item,index}) => {
+        return(
+          <View>
+            <Pressable className="bg-amber-600">
+              <Text key={index}>{item.title}</Text>
+            </Pressable>
+          </View>
+        )
+      }}/>
+    </SafeAreaView>
   );
 }
