b8969f5f0132482883aa6e7fd7696f40e39f38f0
JScoder4005 <muk4005@gmail.com>
2025-10-06 20:46:09 +0530

    Add Appwrite configuration and environment variables; include react-native-appwrite and react-native-url-polyfill dependencies

---
+++ lib/appwrite.ts
@@ -0,0 +1,5 @@
+export const appwriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  Platform: "com.muk.foodordering",
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+};
