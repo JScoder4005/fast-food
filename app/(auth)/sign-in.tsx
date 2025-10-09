f4b5967fd7dc6b70a62a70e5295e6242058365a6
JScoder4005 <muk4005@gmail.com>
2025-10-09 23:45:30 +0530

    Enhance SignIn component; implement form handling and submission logic with validation

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,29 +1,53 @@
 import CustomButton from "@/components/customButton";
 import CustomInput from "@/components/customInput";
-import { Link } from "expo-router";
-import React from "react";
-import { Text, View } from "react-native";
+import { Link, router } from "expo-router";
+import React, { useState } from "react";
+import { Alert, Text, View } from "react-native";
 
 const SignIn = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [form, setForm] = useState({ email: "", password: "" });
+
+  const onSubmit = async () => {
+    if (!form.email || !form.password)
+      return Alert.alert("Error", "Please fill all the fields");
+
+    setIsSubmitting(true);
+    try {
+      //Call Appwrite Sign In function here
+      Alert.alert("Success", "Signed In Successfully");
+      router.replace("/");
+    } catch (error: unknown) {
+      console.log(error);
+      Alert.alert("Error", "Something went wrong, please try again.");
+      setIsSubmitting(false);
+    }
+  };
   return (
     <View className="gap-10 bg-white rounded-lg p-5 mt-5">
       <CustomInput
         placeholder="Enter your email"
         label="Email"
         keyboardType="email-address"
-        onChangeText={(text) => console.log(text)}
-        value=""
+        onChangeText={(text) => setForm((prev) => ({ ...prev, email: text }))}
+        value={form.email}
       />
       <CustomInput
         placeholder="Enter your password"
         label="Password"
         secureTextEntry={true}
-        onChangeText={(text) => console.log(text)}
-        value=""
+        onChangeText={(text) =>
+          setForm((prev) => ({ ...prev, password: text }))
+        }
+        value={form.password}
       />
-      <CustomButton title="Sign In" />
+      <CustomButton
+        title="Sign In"
+        isLoading={isSubmitting}
+        onPress={onSubmit}
+      />
 
-      <View>
+      <View className="flex flex-row justify-center mt-5 gap-2">
         <Text className="base-regular text-gray-100">
           Dont have an account?
         </Text>
