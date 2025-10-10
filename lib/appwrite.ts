348d3af8a72ba89c1fef8a33ddfe68451aca5507
JScoder4005 <muk4005@gmail.com>
2025-10-10 22:00:47 +0530

    Refactor appwrite configuration; ensure mandatory environment variables are defined and improve createUser function error handling

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,7 +1,34 @@
+import { CreateUserPrams } from "@/types";
+import { Account, Avatars, Client, Databases } from "react-native-appwrite";
+
 export const appwriteConfig = {
-  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!,
   Platform: "com.muk.foodordering",
-  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
   databaseId: "68e3f385002faba85bbf",
   userCollectionId: "user",
+};
+
+export const client = new Client();
+
+client
+  .setEndpoint(appwriteConfig.endpoint)
+  .setProject(appwriteConfig.projectId)
+  .setPlatform(appwriteConfig.Platform);
+
+export const account = new Account(client);
+export const databases = new Databases(client);
+
+const avatars = new Avatars(client);
+
+export const createUser = async ({
+  email,
+  password,
+  name,
+}: CreateUserPrams) => {
+  try {
+  } catch (error) {
+    const newAccount = await account.create();
+    throw new Error(error as string);
+  }
 };
