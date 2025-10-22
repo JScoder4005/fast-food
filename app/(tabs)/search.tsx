0863f3f7afbc6a819f0cdebf47aefbcb8d928c09
JScoder4005 <muk4005@gmail.com>
2025-10-23 00:15:59 +0530

    Refactor Search component to improve menu fetching and layout; integrate getCategories and enhance UI with FlatList

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,22 +1,65 @@
-import { getMenu } from "@/lib/appwrite";
+import CartButton from "@/components/cartButton";
+import { getCategories, getMenu } from "@/lib/appwrite";
 import useAppwrite from "@/lib/useAppwrite";
-import React from "react";
-import { Text } from "react-native";
+import { useLocalSearchParams } from "expo-router";
+import React, { useEffect } from "react";
+import { FlatList, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Search = () => {
+  const { category, query } = useLocalSearchParams<{
+    query: string;
+    category: string;
+  }>();
+
   const { data, refetch, loading } = useAppwrite({
     fn: getMenu,
     params: {
-      category: "",
-      query: "",
-      limit: 8,
+      category,
+      query,
+      limit: 6,
     },
   });
-  console.log(data);
+  // console.log(data);
+
+  const { data: categories } = useAppwrite({ fn: getCategories });
+
+  useEffect(() => {
+    refetch({ category, query, limit: 6 });
+  }, [category, query]);
   return (
-    <SafeAreaView>
-      <Text>Search</Text>
+    <SafeAreaView className="bg-white h-full">
+      <FlatList
+        data={data}
+        renderItem={({ item, index }) => {
+          return (
+            <View className="flex-1 max-w-[48%]">
+              <Text>Menu Card</Text>
+            </View>
+          );
+        }}
+        keyExtractor={(item) => item.$id}
+        numColumns={2}
+        columnWrapperClassName="gap-7"
+        contentContainerClassName="gap-7 px-5 pb-32"
+        ListHeaderComponent={() => (
+          <View className="my-5 gap-5">
+            <View className="flex-between flex-row w-full">
+              <View className="flex-start">
+                <Text className="small-bold uppercase text-primary">
+                  Search
+                </Text>
+                <View className="flex-start flex-row gap-x-1 mt-0.5">
+                  <Text className="paragraph-semibold text-dark-100">
+                    Find Your favorite food
+                  </Text>
+                </View>
+              </View>
+              <CartButton />
+            </View>
+          </View>
+        )}
+      />
     </SafeAreaView>
   );
 };
