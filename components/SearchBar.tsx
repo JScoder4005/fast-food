65d4790b143643d672384237ce4e7509a842930c
JScoder4005 <muk4005@gmail.com>
2025-10-27 02:02:22 +0530

    Refactor Filter and SearchBar components; integrate router for category handling in Filter and enhance SearchBar functionality with query state management

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,10 +1,17 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { useLocalSearchParams } from "expo-router";
+import React, { useState } from "react";
+import { TextInput, View } from "react-native";
 
 const SearchBar = () => {
+  const params = useLocalSearchParams<{ query?: string }>();
+  console.log("par", params);
+  const [query, setQuery] = useState(params.query);
   return (
-    <View>
-      <Text>SearchBar</Text>
+    <View className="searchbar">
+      <TextInput
+        className="flex-1 p-5"
+        placeholder="Search for burgers, pizza..."
+      />
     </View>
   );
 };
