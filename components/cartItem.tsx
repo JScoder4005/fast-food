218f165a0b676018f96db0b74afa7d3ca6b8edef
JScoder4005 <muk4005@gmail.com>
2025-10-29 23:41:26 +0530

    Add cartItem component with basic structure

---
+++ components/cartItem.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const cartItem = () => {
+  return (
+    <View>
+      <Text>cartItem</Text>
+    </View>
+  );
+};
+
+export default cartItem;
